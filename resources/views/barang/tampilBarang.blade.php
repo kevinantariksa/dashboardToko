53dbd1c3f8f1fe153b4eb8217f1a9562736fbbf9
kevinantariksa <kevinantariksa@gmail.com>
2021-06-14 22:47:05 +0700

    fifth commit

--- resources/views/barang/tampilBarang.blade.php
+++ resources/views/barang/tampilBarang.blade.php
@@ -24,8 +24,8 @@
             </form>
             <tr>
                 <th scope="col">ID</th>
-                <th scope="col">Nama Barang</th>
                 <th scope="col">Kode Barang</th>
+                <th scope="col">Nama Barang</th>
                 <th scope="col">Harga Modal</th>
                 <th scope="col">Harga Jual</th>
                 <th scope="col">Stok</th>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,427 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Dashboard</title>
+    <title>Kevin Antariksa </title>
+    <!--
+Template 2109 The Card
+http://www.tooplate.com/view/2109-the-card
+-->
+    <!-- load CSS -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600"
+    />
+    <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="cssPW/bootstrap.min.css" />
+    <!-- https://getbootstrap.com/ -->
+    <link rel="stylesheet" href="slick/slick.css" />
+    <link rel="stylesheet" href="slick/slick-theme.css" />
+    <link rel="stylesheet" href="cssPW/magnific-popup.css" />
+    <link rel="stylesheet" href="cssPW/tooplate-style.css" />
+    <!-- Templatemo style -->
+  </head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <body>
+    <!-- Loader -->
+    <div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <div class="tm-main-container">
+      <div class="tm-top-container">
+        <!-- Menu -->
+        <nav id="tmNav" class="tm-nav">
+          <a class="tm-navbar-menu" href="#">Menu</a>
+          <ul class="tm-nav-links">
+            <li class="tm-nav-item active">
+              <a href="#" data-linkid="0" data-align="right" class="tm-nav-link">Intro</a>
+            </li>
+            <li class="tm-nav-item">
+              <a href="#" data-linkid="1" data-align="right" class="tm-nav-link">About</a>
+            </li>
+            <li class="tm-nav-item">
+              <a
+                href="#"
+                data-linkid="2"
+                data-align="middle"
+                class="tm-nav-link">Work</a>
+            </li>
+            <li class="tm-nav-item">
+              <a
+                rel="nofollow" href="https://www.linkedin.com/in/kevin-antariksa/"
+                class="tm-nav-link external">LinkedIn</a>
+            </li>
+          </ul>
+        </nav>
 
-            .full-height {
-                height: 100vh;
-            }
+        <!-- Site header -->
+        <header class="tm-site-header-box tm-bg-dark">
+          <h1 class="tm-site-title">Kevin Antariksa</h1>
+          <p class="mb-0 tm-site-subtitle">Personal Portfolio</p>
+        </header>
+      </div>
+      <!-- tm-top-container -->
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-
-    </head>
-    <body >
-        
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                    @endauth
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <!-- Site content -->
+            <div class="tm-content">
+              <!-- Section 0 Introduction -->
+              <section class="tm-section tm-section-0">
+                <h2 class="tm-section-title mb-3 font-weight-bold">
+                  Hi, There!
+                </h2>
+                <div class="tm-textbox tm-bg-dark">
+                  <p>
+                    The Card template is last updated on 21 May 2019. Read More button is linked to second page. You can try it now. External link button is added on main menu.
+                  </p>
+                  <p class="mb-0">
+                    Please tell your friends about Tooplate for free templates. This is 70% alpha background.
+                  </p>
                 </div>
-            @endif
+                <a href="#" id="tm_about_link" data-linkid="1" class="tm-link">Read More</a>
+              </section>
 
-            <div class="content">
-                <div class="title m-b-md" >
-                    Welcome!<br>
+              <!-- Section 1 About Me -->
+              <section class="tm-section tm-section-1">
+                <div class="tm-textbox tm-textbox-2 tm-bg-dark">
+                  <h2 class="tm-text-blue mb-4">Work Experience</h2>
+                  <p class="mb-4">
+                   
+                    <ul class=""> 
+                    <li> Asisstant Lecturer at Universitas Atma Jaya Yogyakarta for several courses:</li>
+                     <ul>
+                      <li >
+                        Basic Programming (2016, 2018)
+                      </li>
+                      <li  >
+                        Artificial Intelegence (2017)
+                      </li>
+                      <li  >
+                        Web Application Development (2017)
+                      </li>
+                     </ul>
+                     <br>
+                     <li  >
+                      IT Internship (Technocenter) at Astra Sedaya Finance (2019)
+                    </li>
+                    <br>
+                    <li  >
+                      IT System Development - Analyst Programmer at Astra Sedaya Finance (2020)
+                    </li>
+                    <br>
+                    <li  >
+                      Techno Software Specialist Lead at Astra Sedaya Finance (2021)
+                    </li>
+                    </ul>
+                  </p>
+                  <a
+                    href="#"
+                    id="tm_work_link"
+                    data-linkid="2"
+                    class="tm-link m-0"
+                    >Projects</a
+                  >
                 </div>
-                <div class="links">
-                    <h3 id="pDate"></h3><p id="txt"></p>
+              </section>
+
+              <!-- Section 2 Work (Gallery) -->
+              <section class="tm-section tm-section-2 mx-auto">
+                <div class="grid tm-gallery">
+                  <figure class="effect-goliath tm-gallery-item">
+                    <img src="imgPW/01.jpg" alt="Image 1" class="" />
+                    <figcaption>
+                      <h2>
+                        <span>Grand Atma Hotel</span>
+                        Website - Laravel
+                      </h2>
+                      <p>Hotel Reservation Information Systems (for P3L Class)</p>
+                      <a href="imgPW/01.jpg">View more</a>
+                    </figcaption>
+                  </figure>
+                  <figure class="effect-goliath tm-gallery-item">
+                    <img src="imgPW/02.jpg"  class="" />
+                    <figcaption>
+                      <h2>
+                        <span>Hatespeech Classification</span>
+                        Machine Learning - Python
+                      </h2>
+                      <p>Thesis</p>
+                      <a href="imgPW/02.jpg">View more</a>
+                    </figcaption>
+                  </figure>
+                  <figure class="effect-goliath tm-gallery-item">
+                    <img src="imgPW/03.jpg"  class="" />
+                    <figcaption>
+                      <h2>
+                        <span>Progressive Web App</span>
+                        Built using Vue.Js
+                      </h2>
+                      <p>Achieved trough completing Udemy course</p>
+                      <a href="imgPW/03.jpg">View more</a>
+                    </figcaption>
+                  </figure>
+                  <figure class="effect-goliath tm-gallery-item">
+                    <img src="imgPW/04.jpg"  class="" />
+                    <figcaption>
+                      <h2>
+                        <span>Dashboard Toko</span>
+                        Website - Laravel
+                      </h2>
+                      <p>Simple CRUD for Inventory Management </p>
+                      <a href="imgPW/04.jpg">View more</a>
+                    </figcaption>
+                  </figure>
+                  <figure class="effect-goliath tm-gallery-item">
+                    <img src="imgPW/05.jpg"  class="" />
+                    <figcaption>
+                      <h2>
+                        <span>Work Project</span>
+                      </h2>
+                      <p>Ask me on LinkedIn for more details.</p>
+                      <a href="imgPW/05.jpg">View more</a>
+                    </figcaption>
+                  </figure>
                 </div>
-                
+              </section>
+
+              
+
+              <!-- Section 3 Contact
+              <section class="tm-section tm-section-3 tm-section-left">
+                <form action="" class="tm-contact-form" method="post">
+                  <div class="form-group mb-4">
+                    <input
+                      type="text"
+                      id="contact_name"
+                      name="contact_name"
+                      class="form-control"
+                      placeholder="Name"
+                      required
+                    />
+                  </div>
+                  <div class="form-group mb-4">
+                    <input
+                      type="email"
+                      id="contact_email"
+                      name="contact_email"
+                      class="form-control"
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
+                  <div class="form-group mb-4">
+                    <textarea
+                      rows="4"
+                      id="contact_message"
+                      name="contact_message"
+                      class="form-control"
+                      placeholder="Message"
+                      required
+                    ></textarea>
+                  </div>
+                  <div class="form-group mb-0">
+                    <button type="submit" class="btn tm-send-btn tm-fl-right">
+                      Send
+                    </button>
+                  </div>
+                </form>
+              </section> -->
             </div>
-            
+          </div>
         </div>
-    </body>
+      </div>
+
+      <div class="tm-bottom-container">
+        <!-- Barcode -->
+        <!-- <div class="tm-barcode-box">
+          <img src="img/bar-code.jpg" alt="Bar code" />
+        </div> -->
+
+        <!-- Footer -->
+        <footer>
+          || Copyright &copy; 2021 Kevin Antariksa || 
+                  
+        
+        </footer>
+      </div>
+    </div>
+
+    <script src="jsPW/jquery-1.11.0.min.js"></script>
+    <script src="jsPW/background.cycle.js"></script>
+    <script src="slick/slick.min.js"></script>
+    <script src="jsPW/jquery.magnific-popup.min.js"></script>
     <script>
-        const monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni",
-        "Juli", "Agustus", "September", "Oktober", "November", "Desember"
-        ];
-        const d = new Date();
-        var date = new Date();
-        document.getElementById("pDate").innerHTML = date.getDate()+' ' + ( monthNames[d.getMonth()])+' '+date.getFullYear()+'  - '+ date.getHours()+":"+date.getMinutes();
-    
-        </script>
+      let slickInitDone = false;
+      let previousImageId = 0,
+        currentImageId = 0;
+      let pageAlign = "right";
+      let bgCycle;
+      let links;
+      let eachNavLink;
+
+      window.onload = function() {
+        $("body").addClass("loaded");
+      };
+
+      function navLinkClick(e) {
+        if ($(e.target).hasClass("external")) {
+          return;
+        }
+
+        e.preventDefault();
+
+        if ($(e.target).data("align")) {
+          pageAlign = $(e.target).data("align");
+        }
+
+        // Change bg image
+        previousImageId = currentImageId;
+        currentImageId = $(e.target).data("linkid");
+        bgCycle.cycleToNextImage(previousImageId, currentImageId);
+
+        // Change menu item highlight
+        $(`.tm-nav-item:eq(${previousImageId})`).removeClass("active");
+        $(`.tm-nav-item:eq(${currentImageId})`).addClass("active");
+
+        // Change page content
+        $(`.tm-section-${previousImageId}`).fadeOut(function(e) {
+          $(`.tm-section-${currentImageId}`).fadeIn();
+          // Gallery
+          if (currentImageId === 2) {
+            setupSlider();
+          }
+        });
+
+        adjustFooter();
+      }
+
+      $(document).ready(function() {
+        // Set first page
+        $(".tm-section").fadeOut(0);
+        $(".tm-section-0").fadeIn();
+
+        // Set Background images
+        // https://www.jqueryscript.net/slideshow/Simple-jQuery-Background-Image-Slideshow-with-Fade-Transitions-Background-Cycle.html
+        bgCycle = $("body").backgroundCycle({
+          imageUrls: [
+            "imgPW/photo-02.jpg",
+            "imgPW/photo-03.jpg",
+            "imgPW/photo-04.jpg",
+            "imgPW/photo-05.jpg"
+          ],
+          fadeSpeed: 2000,
+          duration: -1,
+          backgroundSize: SCALING_MODE_COVER
+        });
+
+        eachNavLink = $(".tm-nav-link");
+        links = $(".tm-nav-links");
+
+        // "Menu" open/close
+        if (links.hasClass("open")) {
+          links.fadeIn(0);
+        } else {
+          links.fadeOut(0);
+        }
+
+        $("#tm_about_link").on("click", navLinkClick);
+        $("#tm_work_link").on("click", navLinkClick);
+
+        // Each menu item click
+        eachNavLink.on("click", navLinkClick);
+
+        $(".tm-navbar-menu").click(function(e) {
+          if (links.hasClass("open")) {
+            links.fadeOut();
+          } else {
+            links.fadeIn();
+          }
+
+          links.toggleClass("open");
+        });
+
+        // window resize
+        $(window).resize(function() {
+          // If current page is Gallery page, set it up
+          if (currentImageId === 2) {
+            setupSlider();
+          }
+
+          // Adjust footer
+          adjustFooter();
+        });
+
+        adjustFooter();
+      }); // DOM is ready
+
+      function adjustFooter() {
+        const windowHeight = $(window).height();
+        const topHeight = $(".tm-top-container").height();
+        const middleHeight = $(".tm-content").height();
+        let contentHeight = topHeight + middleHeight;
+
+        if (pageAlign === "left") {
+          contentHeight += $(".tm-bottom-container").height();
+        }
+
+        if (contentHeight > windowHeight) {
+          $(".tm-bottom-container").addClass("tm-static");
+        } else {
+          $(".tm-bottom-container").removeClass("tm-static");
+        }
+      }
+
+      function setupSlider() {
+        let slidesToShow = 4;
+        let slidesToScroll = 2;
+        let windowWidth = $(window).width();
+
+        if (windowWidth < 480) {
+          slidesToShow = 1;
+          slidesToScroll = 1;
+        } else if (windowWidth < 768) {
+          slidesToShow = 2;
+          slidesToScroll = 1;
+        } else if (windowWidth < 992) {
+          slidesToShow = 3;
+          slidesToScroll = 2;
+        }
+
+        if (slickInitDone) {
+          $(".tm-gallery").slick("unslick");
+        }
+
+        slickInitDone = true;
+
+        $(".tm-gallery").slick({
+          dots: true,
+          customPaging: function(slider, i) {
+            var thumb = $(slider.$slides[i]).data();
+            return `<a>${i + 1}</a>`;
+          },
+          infinite: true,
+          prevArrow: false,
+          nextArrow: false,
+          slidesToShow: slidesToShow,
+          slidesToScroll: slidesToScroll
+        });
+
+        // Open big image when a gallery image is clicked.
+        $(".slick-list").magnificPopup({
+          delegate: "a",
+          type: "image",
+          gallery: {
+            enabled: true
+          }
+        });
+      }
+    </script>
+  </body>
 </html>
